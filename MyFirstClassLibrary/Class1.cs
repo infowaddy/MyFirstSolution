e2da498613ca2af706d9695e68551be5a77eccc6
zinmin <zinmin@NYANYEHTET>
2025-01-14 02:39:52 -0600

    Add project files.

---
+++ MyFirstClassLibrary/Class1.cs
@@ -0,0 +1,19 @@
+﻿namespace MyFirstClassLibrary
+{
+    public class Class1
+    {
+        public static string GetMessage(string appType = "")
+        {
+            if (appType.Trim().ToUpper() == "WEB")
+            {
+                return "Hello, World! This is Web App.";
+            }
+            else if (appType.Trim().ToUpper() == "CONSOLE")
+            {
+                return "Hello, World! This is Console App.";
+            }
+
+            return "Hello, World! This is Class Library";
+        }
+    }
+}
---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyFirstClassLibrary;
+
+//Console.WriteLine("Hello, World! This is Console App.");
+Console.WriteLine(Class1.GetMessage("Console"));
+Console.ReadLine();
